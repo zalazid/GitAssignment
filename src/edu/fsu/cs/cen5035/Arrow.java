924927939c6720d048b2511d013d2f8b797d5d6d
Zaid Alzaid <zsa14@my.fsu.edu>
2016-09-21 15:57:15 -0400

    Arrow class has been created
    the Arrow class is created , the class extended BasicWeapon class and it's implemented Weapon interface
    this include the implementing hit function with no parameter and hit function that take one int parameters

--- src/edu/fsu/cs/cen5035/Arrow.java
+++ src/edu/fsu/cs/cen5035/Arrow.java
@@ -1,5 +1,23 @@
+package edu.fsu.cs.cen5035;
 /**
  * Created by zaid on 9/17/16.
  */
-public class Arrow {
+public class Arrow extends BasicWeapon implements Weapon {
+
+    public Arrow() { super(70);}
+
+    @Override
+    public int hit() {
+        return DAMAGE;
+    }
+
+    @Override
+    public int hit(int armor) {
+        int damage = DAMAGE - armor;
+        if (damage < 0) {
+            return 0;
+        }
+        return damage;
+    }
+
 }
