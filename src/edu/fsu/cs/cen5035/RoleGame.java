70ef3e60aed4a320b953d9b112438dbdf89b0e98
Zaid Alzaid <zsa14@my.fsu.edu>
2016-09-21 16:46:40 -0400

    RoleGame.java has been updated
    this update is to show the behavior of the rest of wepons (arrow,magicstaff , axe)with no armor and with 20 armor points

--- src/edu/fsu/cs/cen5035/RoleGame.java
+++ src/edu/fsu/cs/cen5035/RoleGame.java
@@ -12,12 +12,40 @@ package edu.fsu.cs.cen5035;
 public class RoleGame {
 
     public static void main(String[] args) {
+       /*
+       * behavior of weapon with 20 points armors point and with no armor for:
+       * 1- sword
+       * 2- arrow
+       * 3- MagicStaff
+       * 4- Axe
+       * */
+
+       // sword no armor
         Weapon sword = WeaponFactory.getWeapon("sword");
         System.out.println("Sword has " + sword.hit() + " of damage.");
+        // sword 20 armors points
         int armor = 20;
         System.out.println("Swords was able to do " + sword.hit(armor) + " of damage due to an armor with "+armor+ " points.");
-        
-        //TODO: Add the remainig weapons here
 
+       // arrow no armor
+        Weapon arrow = WeaponFactory.getWeapon("arrow");
+        System.out.println("Arrow has " + arrow.hit() + " of damage.");
+        // arrow 20 armor points
+         armor = 20;
+        System.out.println("Arrow was able to do " + arrow.hit(armor) + " of damage due to an armor with "+armor+ " points.");
+
+        //MagicStaff no armor
+        Weapon magicstaff = WeaponFactory.getWeapon("magicstaff");
+        System.out.println("MagicStaff has " + magicstaff.hit() + " of damage.");
+        //MagicStaff 20 armor points
+        armor = 20;
+        System.out.println("MagicStaff was able to do " + magicstaff.hit(armor) + " of damage due to an armor with "+armor+ " points.");
+
+        //Axe no armor
+        Weapon axe = WeaponFactory.getWeapon("axe");
+        System.out.println("Axe has " + axe.hit() + " of damage.");
+        //Axe 20 armor points
+        armor = 20;
+        System.out.println("Axe was able to do " + axe.hit(armor) + " of damage due to an armor with "+armor+ " points.");
     }
 }
